0e6c8d2223c71d0bd24b61f7d5c0817ed886fe34
Raxmatilla G'aybullayev <81888260+raxmatilla270@users.noreply.github.com>
2021-09-21 08:07:07 +0500

    Add files via upload

---
+++ city.php
@@ -0,0 +1,29 @@
+<?php
+ 
+ 
+ 
+$city = array (
+  'OZBEKISTON'=> array (),
+  'Buxoro' => array (
+    0 => '	Buxoro shahri',
+    1 => 'Buxoro tumani',
+    2 => 'Gijduvon tumani',
+    3 => 'Olot tumani',
+    4 => 'Qorakol tumani',
+  ),
+  'Samarqand' => array (
+    0 => 'Bulungur tumani',
+    1 => 'Ishtixon tumani',
+    2 => '	Jomboy tumani',
+    3 => 'Narpay tumani',
+    4 => '	Nurobod tumani',
+  ),
+  'TOSHKENT SHAHRI' => array (
+    0 => 'Bektemir tumani',
+    1 => 'Chilonzor tumani',
+    2 => 'Mirobod tumani',
+    3 => '	Sergeli tumani',
+  ),
+);
+ 
+?>
